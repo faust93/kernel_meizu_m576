ce49b5a88ae8c109ecf71f9ad3c3036536bfbf6d
HRTKernel <support@hacker-kernel.com>
2015-06-15 20:23:55 +0200

    readahead: make context readahead more conservative [Wu Fengguang] This helps performance on moderately dense random reads on SSD.
    Transaction-Per-Second numbers provided by Taobao:
    
                    QPS     case
                    -------------------------------------------------------
                    7536    disable context readahead totally
    w/ patch:       7129    slower size rampup and start RA on the 3rd read
                    6717    slower size rampup
    w/o patch:      5581    unmodified context readahead
    
    Before, readahead will be started whenever reading page N+1 when it happen
    to read N recently.  After patch, we'll only start readahead when *three*
    random reads happen to access pages N, N+1, N+2.  The probability of this
    happening is extremely low for pure random reads, unless they are very
    dense, which actually deserves some readahead.
    
    Also start with a smaller readahead window.  The impact to interleaved
    sequential reads should be small, because for a long run stream, the the
    small readahead window rampup phase is negletable.
    
    The context readahead actually benefits clustered random reads on HDD
    whose seek cost is pretty high.  However as SSD is increasingly used for
    random read workloads it's better for the context readahead to concentrate
    on interleaved sequential reads.
    
    Another SSD rand read test from Miao
    
            # file size:        2GB
            # read IO amount: 625MB
            sysbench --test=fileio                          --max-requests=10000                    --num-threads=1                         --file-num=1                            --file-block-size=64K                   --file-test-mode=rndrd                  --file-fsync-freq=0                     --file-fsync-end=off    run
    
    shows the performance of btrfs grows up from 69MB/s to 121MB/s, ext4 from
    104MB/s to 121MB/s.
    
    Signed-off-by: Wu Fengguang <fengguang.wu@intel.com>
    Tested-by: Tao Ma <tm@tao.ma>
    Tested-by: Miao Xie <miaox@cn.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: flar2 <asegaert@gmail.com>

--- mm/readahead.c
+++ mm/readahead.c
@@ -400,7 +400,7 @@ static int try_context_readahead(struct address_space *mapping,
 	 * no history pages:
 	 * it could be a random read
 	 */
-	if (!size)
+	if (!size <= req_size)
 		return 0;
 
 	/*
@@ -411,8 +411,8 @@ static int try_context_readahead(struct address_space *mapping,
 		size *= 2;
 
 	ra->start = offset;
-	ra->size = get_init_ra_size(size + req_size, max);
-	ra->async_size = ra->size;
+	ra->size = min(size + req_size, max);
+	ra->async_size = 1;
 
 	return 1;
 }
