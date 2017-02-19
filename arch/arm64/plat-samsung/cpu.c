322ef68545d1636bfbda26bf577ba18e3727e6ba
faust93 <monumentum@gmail.com>
2017-02-19 13:44:58 +0300

    sock id detection from recent 7420 kernel

--- arch/arm64/plat-samsung/cpu.c
+++ arch/arm64/plat-samsung/cpu.c
@@ -14,10 +14,11 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/io.h>
-
+#include <linux/smc.h>
 
 #include <mach/map.h>
 #include <plat/cpu.h>
+#include <mach/cpuid.h>
 
 unsigned long samsung_cpu_id;
 static unsigned int samsung_cpu_rev;
@@ -52,6 +53,34 @@ void __init s3c64xx_init_cpu(void)
 	samsung_cpu_rev = 0;
 }
 
+#if defined(CONFIG_SOC_EXYNOS7420)
+void __init s5p_init_cpu(void __iomem *cpuid_addr)
+{
+	unsigned long val = 0;
+	int ret = 0;
+
+	ret = exynos_smc_readsfr(CPUID_ADDR, &val);
+	if (ret)
+		panic("cpuid error\n");
+	else {
+		samsung_cpu_id = val;
+		samsung_cpu_rev = samsung_cpu_id & 0xFF;
+	}
+
+	ret = exynos_smc_readsfr(CPUID_ADDR + CHIPID0_OFFSET, &val);
+	if (ret)
+		panic("chipid0 error\n");
+	else
+		samsung_chip_id[0] = (unsigned int)val;
+
+	ret = exynos_smc_readsfr(CPUID_ADDR + CHIPID1_OFFSET, &val);
+	if (ret)
+		panic("chipid1 error\n");
+	else
+		samsung_chip_id[1] = (unsigned int)val & 0xFFFF;
+
+}
+#else
 void __init s5p_init_cpu(void __iomem *cpuid_addr)
 {
 	samsung_cpu_id = __raw_readl(cpuid_addr);
@@ -60,3 +89,4 @@ void __init s5p_init_cpu(void __iomem *cpuid_addr)
 	samsung_chip_id[0] = __raw_readl(cpuid_addr + CHIPID0_OFFSET);
 	samsung_chip_id[1] = __raw_readl(cpuid_addr + CHIPID1_OFFSET) & 0xFFFF;
 }
+#endif
